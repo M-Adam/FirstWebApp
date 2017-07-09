488202cc95ea4444407057879c4840a1fc6488a2
maly <malec.adrian@hotmail.com>
2017-07-09 15:16:28 +0200

    step 1

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var Kontakty = new List<Kontakt> (new Kontakt { Imie = "Adam", Nazwisko = "Nowak", Ulica = "Bursztynowa", Miasto = "Lublin" },
+                new Kontakt { Imie = "Jan", Nazwisko = "Kowalski", Ulica = "Bursztynowa", Miasto = "Lublin" },
+                new Kontakt { Imie = "Roman", Nazwisko = "Sredni", Ulica = "Bursztynowa", Miasto = "Lublin" },
+                new Kontakt { Imie = "Roman", Nazwisko = "Sredni", Ulica = "Bursztynowa", Miasto = "Lublin" }
+                );
+            ViewBag.Kontakty = Kontakty;
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
